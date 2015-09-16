d76be71eb64ac2f7393314ab482bf3563d10af07
alphaleonis <alphaleonis@users.noreply.github.com>
2015-09-16 19:21:56 +0200

    Added some utility base classes for IVsSingleFileGenerator implementations

---
+++ SingleFileGenerator/BaseRoslynCodeGenerator.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.VisualStudio.ComponentModelHost;
+using Microsoft.VisualStudio.LanguageServices;
+using System;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace Alphaleonis.Vsx
+{
+   [ComVisible(true)]
+   public abstract class BaseRoslynCodeGenerator : BaseTextCodeGenerator
+   {      
+      protected sealed override void GenerateCode(string inputFilePath, string inputFileContent, TextWriter writer)
+      {
+         Microsoft.VisualStudio.Shell.ThreadHelper.JoinableTaskFactory.Run(async () =>
+         {
+            IComponentModel componentModel = (IComponentModel)Microsoft.VisualStudio.Shell.Package.GetGlobalService(typeof(SComponentModel));
+            VisualStudioWorkspace workspace = componentModel.GetService<VisualStudioWorkspace>();
+
+            if (workspace == null)
+               throw new InvalidOperationException($"Unable to get the service {nameof(VisualStudioWorkspace)} from the host application.");
+
+            Solution solution = workspace.CurrentSolution;
+            if (solution == null)
+               throw new InvalidOperationException($"No solution found in the current workspace.");
+
+            DocumentId documentId = solution.GetDocumentIdsWithFilePath(inputFilePath).FirstOrDefault();
+
+            if (documentId == null)
+               throw new TextFileGeneratorException(String.Format("Unable to find a document matching the file path \"{0}\".", inputFilePath));
+
+            Document document = solution.GetDocument(documentId);
+
+            document = await GenerateCodeAsync(document);
+
+            await writer.WriteLineAsync((await document.GetTextAsync()).ToString());
+         });
+      }
+      
+      protected abstract Task<Document> GenerateCodeAsync(Document inputDocument);      
+   }
+}
---
+++ SingleFileGenerator/TextFileGeneratorException.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Alphaleonis.Vsx
+{
+
+   [Serializable]
+   public class TextFileGeneratorException : Exception
+   {
+      public TextFileGeneratorException()
+      {
+         Line = -1;
+         Column = -1;
+      }
+
+      public TextFileGeneratorException(int line, string message) : this(line, -1, message) { }
+
+      public TextFileGeneratorException(int line, int column, string message)
+         : base(message)
+      {
+         Line = line;
+         Column = column;
+      }
+
+      public TextFileGeneratorException(int line, int column, string message, Exception inner)
+         : base(message, inner)
+      {
+         Line = line;
+         Column = column;
+      }
+
+      public TextFileGeneratorException(string message) : this(-1, -1, message) { }
+      public TextFileGeneratorException(string message, Exception inner) : this(-1, -1, message, inner) { }
+
+      protected TextFileGeneratorException(SerializationInfo info, StreamingContext context) : base(info, context)
+      {
+         Line = info.GetInt32(nameof(Line));
+         Column = info.GetInt32(nameof(Column));
+      }
+
+      public int Line
+      {
+         get;
+      }
+
+      public int Column
+      {
+         get;
+      }
+
+      public override void GetObjectData(SerializationInfo info, StreamingContext context)
+      {
+         base.GetObjectData(info, context);
+         info.AddValue(nameof(Line), Line);
+         info.AddValue(nameof(Column), Column);
+      }
+   }
+}
